92b487702153b8abfdb1f5b4d21c3748aef4b842
arosa822 <h.rosa822@gmail.com>
2019-06-21 22:52:04 +0000

    add dashboard

--- test/server/getData.py
+++ test/server/getData.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 import psycopg2
 from config import config
+import datetime
+import pytz
 params = config('testData.ini')
 
 def getData_1():
@@ -54,6 +56,7 @@ def iter_row(cursor, size=10):
 def getData_3():
     """ query device data using the fetchmany function"""
     conn = None
+    data = []
     try:
         conn = psycopg2.connect(**params)
         cur = conn.cursor()
@@ -64,6 +67,8 @@ def getData_3():
                 """)
         for row in iter_row(cur,10):
             print(row)
+            data.append(row)
+
         print("The number of fields:", cur.rowcount)
         cur.close()
 
@@ -73,6 +78,38 @@ def getData_3():
         if conn is not None:
             conn.close()
 
+    return data
+
+def getData_4(device_filter):
+    """ query device data using the fetchmany function"""
+    conn = None
+    # create empty dictionary where x = timestamp(utc) && y = state 
+    data = {'x':[],'y':[]}
+    # SQL query we are going to use to get the data
+    sql = """SELECT datetime, value FROM pythontest WHERE device = '{}' ORDER BY datetime"""
+
+    # perform the SQL query using the fetchmany method since we are pulling in
+    # a large dataset
+    try:
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute(sql.format(device_filter))
+        
+        for row in iter_row(cur,10):
+            #print(row)
+            data['x'].append(row[0])
+            data['y'].append(row[1])
+
+        print("The number of fields:", cur.rowcount)
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return data
 
 
 if __name__ == '__main__':
